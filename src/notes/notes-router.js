e6921155ec8cbb587d4fbf3c2efc14464a787d90
Ron Martin <ronmartin89@gmail.com>
2020-03-03 11:54:06 -0800

    finished the '/' endpoint for our notesRouter

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -8,7 +8,47 @@ const notesRouter = express.Router();
 const jsonParser = express.json();
 
 const serializeNote = note => ({
-    id: note.id,
-    note_name: xss(note.note_name),
-    content: xss(note.content),
+  id: note.id,
+  note_name: xss(note.note_name),
+  content: xss(note.content),
+  date_modified: note.date_modified,
+  folder_id: note.folder_id
 });
+
+
+notesRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db');
+    NotesService.getAllNotes(knexInstance)
+      .then(notes => {
+        res.json(notes.map(serializeNote));
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { note_name, content, folder_id } = req.body;
+    const newNote = { note_name, content };
+
+    for (const [key, value] of Object.entries(newNote)) {
+      if (value == null) { // eslint-disable-line eqeqeq
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body`}
+        });
+      }
+    }
+
+    newNote.folder_id = folder_id;
+
+    NotesService.insertNote(
+      req.app.get('db'),
+      newNote
+    )
+      .then(note => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `${note.id}`))
+          .json(serializeNote(note));
+      })
+      .catch(next);
+  });
