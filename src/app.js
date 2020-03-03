c89b268ba48dd8dc3de87d39b59737f3209a1503
Ron Martin <ronmartin89@gmail.com>
2020-03-03 12:04:23 -0800

    finished the Express Server for the Noteful API.

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-const { NODE_ENV } = require('./config');
 
+const { NODE_ENV } = require('./config');
+const foldersRouter = require('./folders/folders-router');
+const notesRouter = require('./notes/notes-router');
 
 const app = express();
 
@@ -13,6 +15,9 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use('/api/folders', foldersRouter);
+app.use('/api/notes', notesRouter);
 
 app.get('/', (req, res, next) => { // eslint-disable-line no-unused-vars
   res.send('Hello, world!');
