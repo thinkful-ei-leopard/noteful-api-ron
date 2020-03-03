9c7191889742e3cc1822c8f482351397e39b0b06
Ron Martin <ronmartin89@gmail.com>
2020-03-03 11:47:20 -0800

    working on notes-router

---
+++ src/notes/notes-router.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const xss = require('xss');
+
+const NotesService = require('./notes-service');
+
+const notesRouter = express.Router();
+const jsonParser = express.json();
+
+const serializeNote = note => ({
+    id: note.id,
+    note_name: xss(note.note_name),
+    content: xss(note.content),
+});
