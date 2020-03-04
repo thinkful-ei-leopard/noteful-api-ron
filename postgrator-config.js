99aff462f85e92c31f2738c9e102de43786d7c9d
Ron Martin <ronmartin89@gmail.com>
2020-03-04 01:35:09 -0800

    updated postgrator-config to take an SSL setting and added a production migration script to package.json

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,4 +1,4 @@
-ATABASErequire('dotenv').config();
+require('dotenv').config();
 
 module.exports = {
   'migrationDirectory': 'migrations',
@@ -6,4 +6,5 @@ module.exports = {
   'connectionString': (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
+  'ssl': !!process.env.SSL,  //This coerces the string "true" to the boolean true and empty to false
 };
